fd0ca5ad5ef6600d42322e59af3b258fff1773f3
Madamot <adam@madamot.com>
2020-06-30 17:45:59 +0100

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,6 +86,13 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-127738270-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
