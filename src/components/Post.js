110f2a3675fdba75243f5f7d365cc74cdd421707
Madamot <adam@madamot.com>
2020-10-18 23:03:45 +0100

    post tag fix

--- src/components/Post.js
+++ src/components/Post.js
@@ -13,6 +13,14 @@ const Post = ({post}) =>
     <ul style={{
       display: 'flex',
       flex: '1',
+      marginLeft: '0',
+      marginRight: '0',
+      marginTop: '0',
+      paddingBottom: '0',
+      paddingLeft: '0',
+      paddingRight: '0',
+      paddingTop: '0',
+      marginBottom: '0',
     }}>
       {post.tags.map(tag => (
         <li style={{listStyleType: 'none'}}>
